6010c49141eeddcd196262d405918f1632f9bfaf
Rogelio Martinez <rmartinez5926.rm@gmail.com>
2021-12-10 21:41:23 -0500

    clean up menu items

--- src/components/Header.js
+++ src/components/Header.js
@@ -28,14 +28,15 @@ function Header() {
                 <CloseWrapper>
                     <CustomClose onClick={()=>setBurgerStatus(false)}/>
                 </CloseWrapper>
+                {cars && cars.map((car, index)=>
+                <li><a key={index} href="#">{car}</a></li>
+                )}
                 <li><a href="#">Existing Inventory</a></li>
                 <li><a href="#">Used Inventory</a></li>
                 <li><a href="#">Trade-in</a></li>
                 <li><a href="#">Cybertruck</a></li>
                 <li><a href="#">Roadster</a></li>
                 <li><a href="#">Semi</a></li>
-                <li><a href="#">Existing Inventory</a></li>
-                <li><a href="#">Existing Inventory</a></li>
             </BurgerNav>
         </Container>
     )
